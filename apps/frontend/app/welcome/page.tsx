785d9b5f2dc1410ec927f2bcc8684c442230097f
Julian Perassi <83669055+perassijulian@users.noreply.github.com>
2025-07-12 13:37:03 -0300

    redirects to new-wallet/create from /welcome and better placeholder for search and sort by

--- apps/frontend/app/welcome/page.tsx
+++ apps/frontend/app/welcome/page.tsx
@@ -1,6 +1,7 @@
-import AccountCard from "@/components/accounts/AccountCard";
 import AccountList from "@/components/accounts/AccountList";
 import Button from "@/components/UI/Button";
+import { ArrowUpDown } from "lucide-react";
+import Link from "next/link";
 
 export default function Welcome() {
   const accounts = [
@@ -12,12 +13,20 @@ export default function Welcome() {
       <div className="max-w-3xl w-full">
         <div className="mt-8 flex flex justify-between items-center">
           <h2 className="font-bold text-3xl">Accounts</h2>
-          <Button>Create wallet</Button>
+          <Link href={"/new-wallet/create"}>
+            <Button>Create wallet</Button>
+          </Link>
         </div>
         <div className="bg-bgSubtle w-full p-4 mt-10 rounded shadow-xl">
-          <div className="flex justify-between px-2">
-            <div>search</div>
-            <div>sort</div>
+          <div className="flex justify-between gap-4">
+            <input
+              type="text"
+              placeholder="Search..."
+              className="w-full border border-border px-2 py-1 rounded text-sm bg-bg"
+            />
+            <button className="flex items-center gap-1 px-3 py-1 text-sm bg-muted rounded whitespace-nowrap">
+              Sort by <ArrowUpDown className="w-4 h-4" />
+            </button>
           </div>
           <div>
             <AccountList accounts={accounts} />
